ec70f70ca12a3749e1f9970333baf55aee73c09e
johadahl <johan.dahl.92@hotmail.com>
2019-08-27 03:44:40 +0200

    Using different template

---
+++ src/components/Header/Header.js
@@ -0,0 +1,188 @@
+import React, { useState } from "react";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  InputBase,
+  Menu,
+  MenuItem,
+  Fab,
+} from "@material-ui/core";
+import {
+  Menu as MenuIcon,
+  NotificationsNone as NotificationsIcon,
+  Person as AccountIcon,
+  Lock as LogOutIcon,
+  Send as SendIcon,
+  ArrowBack as ArrowBackIcon,
+} from "@material-ui/icons";
+import classNames from "classnames";
+
+// styles
+import useStyles from "./styles";
+
+// components
+import { Badge, Typography } from "../Wrappers/Wrappers";
+import Notification from "../Notification/Notification";
+import UserAvatar from "../UserAvatar/UserAvatar";
+
+// context
+import {
+  useLayoutState,
+  useLayoutDispatch,
+  toggleSidebar,
+} from "../../context/LayoutContext";
+import { useUserDispatch, signOut } from "../../context/UserContext";
+
+const notifications = [
+  { id: 0, color: "warning", message: "Check out this awesome ticket" },
+  {
+    id: 1,
+    color: "success",
+    type: "info",
+    message: "What is the best way to get ...",
+  },
+  {
+    id: 2,
+    color: "secondary",
+    type: "notification",
+    message: "This is just a simple notification",
+  },
+  {
+    id: 3,
+    color: "primary",
+    type: "e-commerce",
+    message: "12 new orders has arrived today",
+  },
+];
+
+export default function Header(props) {
+  var classes = useStyles();
+
+  // global
+  var layoutState = useLayoutState();
+  var layoutDispatch = useLayoutDispatch();
+  var userDispatch = useUserDispatch();
+
+  // local
+  var [mailMenu, setMailMenu] = useState(null);
+  var [isMailsUnread, setIsMailsUnread] = useState(true);
+  var [notificationsMenu, setNotificationsMenu] = useState(null);
+  var [isNotificationsUnread, setIsNotificationsUnread] = useState(true);
+  var [profileMenu, setProfileMenu] = useState(null);
+
+  return (
+    <AppBar position="fixed" className={classes.appBar}>
+      <Toolbar className={classes.toolbar}>
+        <IconButton
+          color="inherit"
+          onClick={() => toggleSidebar(layoutDispatch)}
+          className={classNames(
+            classes.headerMenuButton,
+            classes.headerMenuButtonCollapse,
+          )}
+        >
+          {layoutState.isSidebarOpened ? (
+            <ArrowBackIcon
+              classes={{
+                root: classNames(
+                  classes.headerIcon,
+                  classes.headerIconCollapse,
+                ),
+              }}
+            />
+          ) : (
+            <MenuIcon
+              classes={{
+                root: classNames(
+                  classes.headerIcon,
+                  classes.headerIconCollapse,
+                ),
+              }}
+            />
+          )}
+        </IconButton>
+        <Typography variant="h6" weight="medium" className={classes.logotype}>
+          Volkswagen Group Sweden - Lost Sales Analytics
+        </Typography>
+        <div className={classes.grow} />
+        <IconButton
+          color="inherit"
+          aria-haspopup="true"
+          aria-controls="mail-menu"
+          onClick={e => {
+            setNotificationsMenu(e.currentTarget);
+            setIsNotificationsUnread(false);
+          }}
+          className={classes.headerMenuButton}
+        >
+          <Badge
+            badgeContent={isNotificationsUnread ? notifications.length : null}
+            color="warning"
+          >
+            <NotificationsIcon classes={{ root: classes.headerIcon }} />
+          </Badge>
+        </IconButton>
+        <IconButton
+          aria-haspopup="true"
+          color="inherit"
+          className={classes.headerMenuButton}
+          aria-controls="profile-menu"
+          onClick={e => setProfileMenu(e.currentTarget)}
+        >
+          <AccountIcon classes={{ root: classes.headerIcon }} />
+        </IconButton>
+        <Menu
+          id="notifications-menu"
+          open={Boolean(notificationsMenu)}
+          anchorEl={notificationsMenu}
+          onClose={() => setNotificationsMenu(null)}
+          className={classes.headerMenu}
+          disableAutoFocusItem
+        >
+          {notifications.map(notification => (
+            <MenuItem
+              key={notification.id}
+              onClick={() => setNotificationsMenu(null)}
+              className={classes.headerMenuItem}
+            >
+              <Notification {...notification} typographyVariant="inherit" />
+            </MenuItem>
+          ))}
+        </Menu>
+        <Menu
+          id="profile-menu"
+          open={Boolean(profileMenu)}
+          anchorEl={profileMenu}
+          onClose={() => setProfileMenu(null)}
+          className={classes.headerMenu}
+          classes={{ paper: classes.profileMenu }}
+          disableAutoFocusItem
+        >
+          <div className={classes.profileMenuUser}>
+            <Typography variant="h4" weight="medium">
+              Rolf Thomander
+            </Typography>
+          </div>
+          <MenuItem
+            className={classNames(
+              classes.profileMenuItem,
+              classes.headerMenuItem,
+            )}
+          >
+            <AccountIcon className={classes.profileMenuIcon} /> Inställningar
+          </MenuItem>
+          <MenuItem
+            className={classNames(
+              classes.profileMenuItem,
+              classes.headerMenuItem,
+            )}
+            onClick={() => signOut(userDispatch, props.history)}
+          >
+            <LogOutIcon className={classes.profileMenuIcon} /> Logga ut
+          </MenuItem>
+        </Menu>
+      </Toolbar>
+    </AppBar>
+  );
+}
